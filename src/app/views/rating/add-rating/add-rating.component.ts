4c7d6fd9a6ed122e4bb478e060488f8f4275f074
aridhiseif <aridhi.seif@gmail.com>
2022-05-26 22:05:39 +0100

    adding rating module
    completing the rating module and its components

--- src/app/views/rating/add-rating/add-rating.component.ts
+++ src/app/views/rating/add-rating/add-rating.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
-  selector: 'app-add-rating',
-  templateUrl: './add-rating.component.html',
-  styleUrls: ['./add-rating.component.scss']
+    selector: 'app-add-rating',
+    templateUrl: './add-rating.component.html',
+    styleUrls: ['./add-rating.component.scss']
 })
 export class AddRatingComponent implements OnInit {
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
 
-  constructor() { }
+    constructor(private http: HttpClient, public router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    submitForm(value: string, value2: string, value3: string) {
+        const body = {
+            userId: value,
+            conducteurId: value2,
+            rating: parseInt(value3)
+        };
+
+        console.log(body);
+        this.http
+            .post('http://127.0.0.1:3000/ratings', body, this.httpOptions)
+            .subscribe(
+                (response) => console.log(response),
+                (error) => console.log(error)
+            );
+        alert('rating added');
+        this.router.navigate(['/ratings']);
+        return false;
+    }
 }
--- src/app/views/rating/update-rating/update-rating.component.ts
+++ src/app/views/rating/update-rating/update-rating.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
-  selector: 'app-update-rating',
-  templateUrl: './update-rating.component.html',
-  styleUrls: ['./update-rating.component.scss']
+    selector: 'app-update-rating',
+    templateUrl: './update-rating.component.html',
+    styleUrls: ['./update-rating.component.scss']
 })
 export class UpdateRatingComponent implements OnInit {
+    id = 1;
+    dataone;
 
-  constructor() { }
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private http: HttpClient,
+        public router: Router,
+        private actRoute: ActivatedRoute
+    ) {
+        this.id = this.actRoute.snapshot.params.id;
+    }
+    ngOnInit(): void {
+        this.http
+            .get<any[]>(
+                'http://127.0.0.1:3000/ratings/' + this.id,
+                this.httpOptions
+            )
+            .subscribe((res: any) => {
+                this.dataone = res;
+                console.log(this.dataone);
+            });
+    }
 
+    submitForm(value: string, value2: string, value3: string) {
+        const body = {
+            userId: value,
+            conducteurId: value2,
+            rating: parseInt(value3)
+        };
+
+        console.log(body);
+        this.http
+            .put(
+                'http://127.0.0.1:3000/ratings/' + this.id,
+                body,
+                this.httpOptions
+            )
+            .subscribe(
+                (response) => console.log(response),
+                (error) => console.log(error)
+            );
+        alert('rating updated');
+        this.router.navigate(['/ratings']);
+        return false;
+    }
 }
